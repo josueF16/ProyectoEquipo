9c74c5e51e4229a20b3fbc0253c4703eb3ede2ec
sahianm <sahian.marine@uab.edu.bo>
2023-11-02 23:01:31 -0300

    Agregando modelado de titulo

---
+++ PruebaColaboradores/Models/Titulo.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Titulo
+    {
+        [Key]
+        public int IdTitulo { get; set; }
+        public string titulo { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
